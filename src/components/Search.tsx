aaf98668d27d5ca510248ea6d65a76898ba1f8cb
KimEunHye <gpqjr00@naver.com>
2023-07-17 20:45:27 +0900

    Feat: 검색 바 스타일 및 검색 시 api 요청기능 추가

---
+++ src/components/Search.tsx
@@ -0,0 +1,68 @@
+import { styled } from 'styled-components';
+import { useEffect, useState } from 'react';
+import { getData } from '../api/api';
+import useDebounce from '../hooks/useDebounce';
+
+import { AiOutlineSearch } from 'react-icons/ai';
+
+const Search = () => {
+  const [searchInput, setSearchInput] = useState<string>('');
+
+  const debouncedSearchInput = useDebounce(searchInput, 350);
+
+  const searchItems = async (search: string) => {
+    const response = await getData(search);
+    console.log(response);
+  };
+
+  useEffect(() => {
+    if (debouncedSearchInput.length !== 0) {
+      searchItems(debouncedSearchInput);
+    }
+  }, [debouncedSearchInput]);
+
+  return (
+    <InputWrap>
+      <InputBox
+        placeholder={`질환명을 입력해 주세요.`}
+        onChange={event => setSearchInput(event.target.value)}
+      />
+      <SearchIcon>
+        <AiOutlineSearch size='30' />
+      </SearchIcon>
+    </InputWrap>
+  );
+};
+
+export default Search;
+
+const InputWrap = styled.div`
+  width: 100vw;
+  height: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
+const InputBox = styled.input`
+  width: 30%;
+  height: 7vh;
+  padding-left: 30px;
+  font-size: 20px;
+  background: #fff;
+  border: 1px solid black;
+  border-radius: 30px;
+`;
+
+const SearchIcon = styled.div`
+  width: 2.7vw;
+  height: 5.4vh;
+  position: absolute;
+  left: 62.5%;
+  border-radius: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
