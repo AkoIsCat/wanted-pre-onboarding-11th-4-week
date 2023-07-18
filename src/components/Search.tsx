6b29aa72ea085735c6e6150cfa6c4565a4a0b024
KimEunHye <gpqjr00@naver.com>
2023-07-18 20:17:38 +0900

    Feat: Search 컴포넌트 구조 변경 1. SearchResult 컴포넌트를 Home에서 Search 하위 컴포넌트로 이동 2. keyDownHandler 함수 추가및 추천 검색어 이동 기능 구현

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,15 +2,21 @@ import { styled } from 'styled-components';
 import { useEffect, useState } from 'react';
 import { getData } from '../api/api';
 import useDebounce from '../hooks/useDebounce';
-import { useRecommendSearchDispatch } from '../store/SuggestedSearch-context';
+import {
+  useRecommendSearchDispatch,
+  useRecommendSearchState,
+} from '../store/RecommendSearch-context';
 
 import { AiOutlineSearch } from 'react-icons/ai';
+import SearchResult from './SearchResult';
 
 const Search = () => {
   const [searchInput, setSearchInput] = useState<string>('');
+  const [idx, setIndex] = useState<number>(-1);
   const dispatch = useRecommendSearchDispatch();
 
   const debouncedSearchInput = useDebounce(searchInput, 350);
+  const recommendData = useRecommendSearchState();
 
   const searchItems = async (search: string) => {
     if (search === '') {
@@ -31,18 +37,45 @@ const Search = () => {
       searchItems(debouncedSearchInput);
     } else {
       searchItems('');
+      setIndex(-1);
     }
   }, [debouncedSearchInput]);
+
+  const keyDownHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    switch (e.key) {
+      case 'ArrowDown':
+        setIndex(prevIdx => (prevIdx + 1) % recommendData.length);
+        break;
+      case 'ArrowUp':
+        setIndex(prevIdx => (prevIdx - 1 + 7) % recommendData.length);
+        break;
+      case 'Escape':
+        searchItems('');
+        setSearchInput('');
+        setIndex(-1);
+        break;
+      case 'Enter':
+        if (idx !== -1 && recommendData[idx]) {
+          const data = recommendData[idx].sickNm;
+          searchItems(data);
+          setSearchInput(data);
+        }
+        break;
+    }
+  };
 
   return (
     <InputWrap>
       <InputBox
         placeholder={`질환명을 입력해 주세요.`}
         onChange={event => setSearchInput(event.target.value)}
+        onKeyDown={keyDownHandler}
+        value={searchInput}
       />
       <SearchIcon>
         <AiOutlineSearch size='30' />
       </SearchIcon>
+      <SearchResult focusIndex={idx} />
     </InputWrap>
   );
 };
@@ -53,6 +86,7 @@ const InputWrap = styled.div`
   width: 100vw;
   height: auto;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   position: relative;
@@ -73,6 +107,7 @@ const SearchIcon = styled.div`
   height: 5.4vh;
   position: absolute;
   left: 62.5%;
+  top: 9px;
   border-radius: 100%;
   display: flex;
   align-items: center;
