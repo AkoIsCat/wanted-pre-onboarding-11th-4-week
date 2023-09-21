154a606c088e875f59576726bf1a197ad03c30e7
KimEunHye <gpqjr00@naver.com>
2023-09-21 23:39:59 +0900

    DEL: Context를 사용하지 않아 Provider 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import RecommenedSearchProvider from './store/RecommendSearch-context.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RecommenedSearchProvider>
-    <App />
-  </RecommenedSearchProvider>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
