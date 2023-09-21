a09781e0197b13cc25e0239c4783a5354ebd1f6b
KimEunHye <gpqjr00@naver.com>
2023-09-21 22:45:38 +0900

    Feat: 기존 로컬 스토리지에 데이터를 캐싱하던 기능을 useQuery를 이용해 간편하게 캐싱하도록 수정

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,112 +1,41 @@
 import { styled } from 'styled-components';
 import { useEffect, useState } from 'react';
-import { getData } from '../api/api';
 import useDebounce from '../hooks/useDebounce';
-import {
-  useRecommendSearchDispatch,
-  useRecommendSearchState,
-} from '../store/RecommendSearch-context';
 
 import { AiOutlineSearch } from 'react-icons/ai';
 import SearchResult from './SearchResult';
+import { useSearchResult } from '../hooks/useSearchResult';
 
 const Search = () => {
   const [searchInput, setSearchInput] = useState<string>('');
   const [idx, setIndex] = useState<number>(-1);
-  const dispatch = useRecommendSearchDispatch();
 
   const debouncedSearchInput = useDebounce(searchInput, 350);
-  const recommendData = useRecommendSearchState();
+
+  const { data } = useSearchResult(debouncedSearchInput);
 
   useEffect(() => {
-    if (debouncedSearchInput.trim().length !== 0) {
-      searchItems(debouncedSearchInput);
-    } else {
-      searchItems('');
+    if (debouncedSearchInput.trim().length === 0) {
       setIndex(-1);
     }
   }, [debouncedSearchInput]);
 
-  const cachingLocalstorage = (result: any, input: string, tts: number) => {
-    const now = new Date();
-    const cachedData = {
-      result,
-      expiry: now.getTime() + tts,
-    };
-    localStorage.setItem(input, JSON.stringify(cachedData));
-  };
-
-  const removeExpiredCaches = () => {
-    const now = new Date();
-    for (let i = 0; i < localStorage.length; i++) {
-      const key = localStorage.key(i);
-      if (key) {
-        const cachingData = localStorage.getItem(key);
-        if (cachingData) {
-          const parseData = JSON.parse(cachingData);
-          if (now.getTime() > parseData.expiry) {
-            localStorage.removeItem(key);
-          }
-        }
-      }
-    }
-  };
-
-  if (localStorage.length !== 0) {
-    setInterval(() => {
-      removeExpiredCaches();
-    }, 60000);
-  }
-
-  const searchItems = async (search: string) => {
-    if (search === '') {
-      dispatch({ type: 'UPDATE', payload: { expiry: 0, result: [] } });
-      return;
-    }
-
-    const trimInput = search.trim();
-    const cachingData = localStorage.getItem(trimInput);
-
-    if (cachingData) {
-      const parseData = JSON.parse(cachingData);
-      dispatch({ type: 'UPDATE', payload: parseData });
-    } else {
-      try {
-        const response = await getData(trimInput);
-        const now = new Date();
-        const payload =
-          response.length > 7
-            ? { result: response.slice(0, 7), expiry: now.getTime() }
-            : { result: response, expiry: now.getTime() };
-
-        dispatch({ type: 'UPDATE', payload });
-        cachingLocalstorage(payload.result, trimInput, 5000);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    }
-  };
-
   const keyDownHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
     switch (e.key) {
       case 'ArrowDown':
-        setIndex(prevIdx => (prevIdx + 1) % recommendData.result.length);
+        setIndex(prevIdx => (prevIdx + 1) % data!.length);
         break;
       case 'ArrowUp':
-        setIndex(
-          prevIdx => (prevIdx - 1 + recommendData.result.length) % recommendData.result.length
-        );
+        setIndex(prevIdx => (prevIdx - 1 + data!.length) % data!.length);
         break;
       case 'Escape':
-        searchItems('');
         setSearchInput('');
         setIndex(-1);
         break;
       case 'Enter':
-        if (idx !== -1 && recommendData.result[idx]) {
-          const data = recommendData.result[idx].sickNm;
-          searchItems(data);
-          setSearchInput(data);
+        if (idx !== -1 && data![idx]) {
+          const enterData = data![idx].sickNm;
+          setSearchInput(enterData);
         }
         break;
     }
@@ -115,7 +44,7 @@ const Search = () => {
   return (
     <InputWrap>
       <InputBox
-        placeholder={`질환명을 입력해 주세요.`}
+        placeholder='질환명을 입력해 주세요.'
         onChange={event => setSearchInput(event.target.value)}
         onKeyDown={keyDownHandler}
         value={searchInput}
@@ -123,7 +52,7 @@ const Search = () => {
       <SearchIcon>
         <AiOutlineSearch size='30' />
       </SearchIcon>
-      <SearchResult focusIndex={idx} />
+      <SearchResult focusIndex={idx} data={data} />
     </InputWrap>
   );
 };
