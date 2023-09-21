bfa7bbfb4259c40b163b9b8b30a5500d6bf3e86e
KimEunHye <gpqjr00@naver.com>
2023-09-21 22:44:18 +0900

    Feat: QueryClient 정의 및 QueryClientProvider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './pages/Home';
 import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     <>
-      <GlobalStyles />
-      <Home />
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyles />
+        <Home />
+      </QueryClientProvider>
     </>
   );
 }
